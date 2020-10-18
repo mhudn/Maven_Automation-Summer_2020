9c7e5e4907a34e2486f273f28e7dd4b38339711d
mhudn <mohi81635@gmail.com>
2020-10-18 11:31:56 -0400

    pushing in class Assignment (Yahoo)

---
+++ Automation_Java/src/test/java/Action_Items/in_class_Assignment2.java
@@ -0,0 +1,43 @@
+package Action_Items;
+
+import Reusable_Libraries.Abstract_Class;
+import Reusable_Libraries.Reusable_Methods;
+import Reusable_Libraries.Reusable_Methods_Loggers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class in_class_Assignment2 extends Abstract_Class
+{
+    @Test
+    public void YahooInClass() throws InterruptedException
+    {
+        Reusable_Methods_Loggers.checkTitle(driver,"https://yahoo.com","Yahoo");
+
+        driver.navigate().to("https://yahoo.com");
+        Thread.sleep(2000);
+
+
+        List<WebElement> linkCount = driver.findElements(By.xpath("//*[@class='_yb_e0gik']"));
+
+        System.out.println("Link count is:" + linkCount.size());
+
+        Reusable_Methods_Loggers.sendKeys(driver,"//*[@name='p']","Nutrition",logger,"Nutrition in search box");
+        Reusable_Methods_Loggers.submit(driver,"//*[@value='Search']",logger," Hitting Enter");
+        Thread.sleep(2000);
+
+
+
+
+
+        /*//clicking on sign in button
+        Reusable_Methods.click(driver,"//*[@class='_yb_687jt']","Sign In button");
+        Thread.sleep(2000);
+
+        //Stay Sign in checkbox
+        boolean checkboxState = driver.findElement(By.xpath("//*[@id='persistent']")).isSelected();*/
+    }
+}
